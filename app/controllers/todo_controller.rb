1c87f6ad7a4eea9bd1012335de847c58358817e0
Danny Mejia <mejiadanny488@gmail.com>
2017-09-30 17:14:46 +0000

    Declare instance variable

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,9 @@
 class TodoController < ApplicationController 
     def index
     end
+   
+    def show
+        @todo_description = "Visit recycling center"
+        @todo_pomodoro_estimate = "6"
+    end
 end
