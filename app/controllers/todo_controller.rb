5917327abe6c48432367486557dcf340dfe8e4e0
Danny Mejia <mejiadanny488@gmail.com>
2017-09-30 19:23:22 +0000

    If statments

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,7 +3,13 @@ class TodoController < ApplicationController
     end
    
     def show
-        @todo_description = "Visit recycling center"
-        @todo_pomodoro_estimate = "6"
+        todo_id = '2'
+        if todo_id == '1'
+           @todo_description = "Visit recycling center"
+           @todo_pomodoro_estimate = "6"
+        elsif todo_id == '2'
+           @todo_description = "Go Home"
+           @todo_pomodoro_estimate = "2"
+        end
     end
 end
