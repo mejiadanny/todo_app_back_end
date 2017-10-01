3e7e3bf80f7314bcc0ecd640a674c72fc674f207
Danny Mejia <mejiadanny488@gmail.com>
2017-10-01 19:49:23 +0000

    Make HTML Smarter - little glitch

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -8,7 +8,7 @@ class TodoController < ApplicationController
          t.description = params['description']
          t.pomodoro_estimate = params['pomodoro_estimate']
          t.save
-    redirect_to "/todo/show/#{ t.id }"
+         redirect_to "/todo/show/#{ t.id }"
     end
     def show
         @todo = Todo.find_by_id(params[:id])
