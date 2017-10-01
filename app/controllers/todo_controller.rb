75d8debc8b7d76c13713af3a5401e08441580e1a
Danny Mejia <mejiadanny488@gmail.com>
2017-10-01 19:01:12 +0000

    Fix underscore pomodoro problem

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,10 +4,10 @@ class TodoController < ApplicationController
     def new
     end
     def create
-    t = Todo.new
-    t.description = params['description']
-    t.pomodoro_estimate = params['pomodoro_estimate']
-    t.save
+         t = Todo.new
+         t.description = params['description']
+         t.pomodoro_estimate = params['pomodoro_estimate']
+         t.save
     redirect_to "/todo/show/#{ t.id }"
     end
     def show
